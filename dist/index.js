a2fd70aeeb7b9ed2d5db518d498c4c6e06b72957
Pawel Pesz <pawel.pesz@lifeworks.com>
2023-10-13 08:41:45 +0100

    Tweaked and reformatted GH action code * Empty string checks can be simplified. * There was minor bug in Session reason construction. * Also updated compiled and cache files.

--- dist/index.js
+++ dist/index.js
@@ -71871,9 +71871,9 @@ const run = async () => {
   try {
     console.log("Start SSM Port Forwarding Session");
     const client = new SSMClient({
-			region: process.env.AWS_DEFAULT_REGION,
-			customUserAgent: "gha".concat("-",github.context.repo.repo)
-	});
+      region: process.env.AWS_DEFAULT_REGION,
+      customUserAgent: "gha".concat("-", github.context.repo.repo)
+    });
 
     const params = SanitizeInputs();
     const command = new StartSessionCommand(params);
@@ -71893,20 +71893,20 @@ function SanitizeInputs() {
   const _localPortNumber = core.getInput('localPortNumber', { required: true });
   const _host = core.getInput('host');
 
-  const _documentName = (_host.length === 0) ? 'AWS-StartPortForwardingSession' : 'AWS-StartPortForwardingSessionToRemoteHost';
+  const _documentName = !_host ? 'AWS-StartPortForwardingSession' : 'AWS-StartPortForwardingSessionToRemoteHost';
 
   const _parameters = new Map();
-  _parameters.set('portNumber', [ _portNumber ]);
-  _parameters.set('localPortNumber', [ _localPortNumber ]);
-  if (_host.length > 0) {
-    _parameters.set('host', [ _host ]);
+  _parameters.set('portNumber', [_portNumber]);
+  _parameters.set('localPortNumber', [_localPortNumber]);
+  if (_host) {
+    _parameters.set('host', [_host]);
   }
 
   return {
     Target: _targetId,
     DocumentName: _documentName,
     Parameters: _parameters,
-    Reason: github.context.serverUrl + "/" + github.context.repo + "/actions/runs/" + github.context.runId.toString()
+    Reason: github.context.serverUrl + "/" + github.context.repo.repo + "/actions/runs/" + github.context.runId.toString()
   };
 }
 
